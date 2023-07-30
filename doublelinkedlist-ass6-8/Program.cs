e2573e59917f155ba05a23d961d278c69eede16d
venkatesheee26@gmail.com <venkatesheee26@gmail.com>
2023-07-30 17:17:16 +0530

    ass

---
+++ doublelinkedlist-ass6-8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Phase1Section4._7c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        public static void runApp()
+        {
+            DoubleLinkedList list = new DoubleLinkedList();
+            Node n = new Node();
+            n.Data = "root";
+            list.add(n);
+            for (int i = 0; i < 10; i++)
+            {
+                Node node = new Node();
+                node.Data = Convert.ToString(i);
+                list.add(node);
+            }
+
+            Console.WriteLine("length of singly linked list=" + list.getLengthOfList());
+            Console.WriteLine("Traversing forward..");
+            n = list.getRoot();
+            while (n != null)
+            {
+                Console.WriteLine(n.Data);
+                n = list.getNext();
+            }
+
+
+            Console.WriteLine("Traversing backwards..");
+            n = list.getPrev();
+            while (n != null)
+            {
+                Console.WriteLine(n.Data);
+                n = list.getPrev();
+            }
+
+        }
+    }
+}
+
---
+++ selectionsort-ass/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Phase1Section4._23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        public static void runApp()
+        {
+            string[] students = new string[10];
+            students[0] = "Karuna";
+            students[1] = "Mark";
+            students[2] = "Advaith";
+            students[3] = "Sangeeta";
+            students[4] = "Nazia";
+            students[5] = "Faisal";
+            students[6] = "Tania";
+            students[7] = "Guru";
+            students[8] = "Chandni";
+            students[9] = "Kamleshwar";
+
+            int n = 10;
+            string temp;
+            int smallest;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                smallest = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (students[j].CompareTo(students[smallest]) < 0)
+                    {
+                        smallest = j;
+                    }
+                }
+                temp = students[smallest];
+                students[smallest] = students[i];
+                students[i] = temp;
+            }
+
+            foreach (string s in students)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
+
